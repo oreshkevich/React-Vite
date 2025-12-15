08e15a409641fc0276926f3d1ee3a17ef9dfa40c
Henadzi Arashkevich <oreshkevich85@mail.ru>
2025-12-15 14:34:33 +0300

    feat: add task hm6

---
+++ src/entities/album/model/types.ts
@@ -0,0 +1,13 @@
+export interface Album {
+  id: number;
+  userId: number;
+  title: string;
+}
+
+export interface Photo {
+  id: number;
+  albumId: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {MouseEventHandler, PropsWithChildren} from 'react';
 import styles from './Button.module.scss';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary';
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({
+export const Button: React.FC<PropsWithChildren<ButtonProps>> = ({
   children,
   variant = 'primary',
+  onClick,
+  disabled,
   ...props
 }) => {
   return (
-    <button className={`${styles.button} ${styles[variant]}`} {...props}>
+    <button
+      className={`${styles.button} ${styles[variant]}`}
+      onClick={onClick}
+      disabled={disabled}
+      {...props}
+    >
       {children}
     </button>
   );
